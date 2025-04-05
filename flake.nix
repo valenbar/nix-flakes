4cbb3ed6413a4e0a7a4cce9e623781db9ae24790
Valentin <valentin.bartschies@gmail.com>
2025-04-06 00:03:17 +0200

    use flake input for togo package aswell

--- flake.nix
+++ flake.nix
@@ -7,15 +7,19 @@
       url = "github:prime-run/go-typer";
       flake = false;
     };
+    togo-src = {
+      url = "github:prime-run/togo";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, go-typer-src }: let
+  outputs = { self, nixpkgs, go-typer-src, togo-src }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
       packages.${system} = {
         hello = pkgs.hello;
-        togo = pkgs.callPackage ./togo/package.nix { };
+        togo = pkgs.callPackage ./togo/package.nix { src = togo-src; };
         go-typer = pkgs.callPackage ./go-typer/package.nix { src = go-typer-src; };
       };
       defaultPackage.${system} = self.packages.${system}.hello;
--- togo/package.nix
+++ togo/package.nix
@@ -1,23 +1,20 @@
 {
-  lib,
   buildGoModule,
-  fetchFromGitHub
+  src ? null
 }:
 buildGoModule (finalAttrs: {
   pname = "togo";
   version = "1.0.1";
-  src = fetchFromGitHub {
-    owner = "prime-run";
-    repo = "togo";
-    rev = "v${finalAttrs.version}";
-    sha256 = "sha256-0wOovQV/2zxqTNNmVFLOfzAivLY1y9+04OnygUo8v1c=";
-  };
+  inherit src;
+
   vendorHash = "sha256-7IPI02EXnEiy2OsysxL0xKZl/YASAo6xBXvUeNjYyfU=";
+
   ldflags = [
     "-s"
     "-w"
   ];
-  meta = with lib; {
+
+  meta = {
     mainProgram = "togo";
     homepage = "https://github.com/prime-run/togo";
   };
